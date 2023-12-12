caf9c7b8e1927ca561192d5d7440b0443f7948a1
Yong Choi <ychoi_kr@naver.com>
2023-12-12 20:25:32 +0900

    display OCR result with rectangles

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ language_options = ["en", "ja", "ko", "fr", "de"]  # 사용 가능한 언어 목
 selected_languages = st.multiselect("언어 선택", language_options, default=["en"])
 
 # 정확도 임계값 설정
-threshold = st.slider("정확도 임계값 설정", 0.0, 1.0, 0.5)
+threshold = st.slider("정확도 임곗값 설정", 0.0, 1.0, 0.5)
 
 # 이미지 업로드
 uploaded_file = st.file_uploader("이미지 업로드", type=["png", "jpg", "jpeg"])
@@ -35,8 +35,17 @@ if uploaded_file is not None:
         reader = easyocr.Reader(selected_languages)
         result = reader.readtext(img)
 
-        # 결과 표시
+        # 사각형 그리기 및 결과 표시
         st.write("OCR 결과:")
         for bbox, text, conf in result:
+            top_left = tuple([int(val) for val in bbox[0]])
+            bottom_right = tuple([int(val) for val in bbox[2]])
             if conf > threshold:
+                # 신뢰도에 따른 색상 설정
+                color = (0, 255, 0) if conf > 0.8 else (255, 0, 0)
+                # 사각형 그리기
+                img = cv2.rectangle(img, top_left, bottom_right, color, 2)
                 st.write(text)
+        
+        # 변환된 이미지 표시
+        st.image(img, caption='Processed Image', use_column_width=True)
